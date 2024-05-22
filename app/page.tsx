1d86606ef5301aff031e0a2645c7239b29673941
Jostin Jarry-Nohair <jostin.jarry-nohair@garda.com>
2024-05-22 11:17:30 -0400

    Build IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LatestIssue from "@/app/LatestIssue";
 import IssueSummary from "@/app/IssueSummary";
 import prisma from "@/prisma/client";
+import IssueChart from "@/app/IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -9,5 +10,5 @@ export default async function Home() {
   });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
