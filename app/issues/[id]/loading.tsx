8d9490ac10d1aa22785e89717579fe6ea7a2ef29
Jostin Jarry-Nohair <jostin.jarry-nohair@garda.com>
2024-05-21 22:44:48 -0400

    Refactor: Organize imports statements

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import ReactMarkdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Box, Card, Flex } from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
+
 const LoadingIssueDetailPage = () => {
   return (
     <Box className="max-w-xl">
